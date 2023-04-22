b2c43daa6a61f6473aff4429f3f940dfa1bc911f
DrEremin <ivaneremin1979@gmail.com>
2023-04-22 17:31:50 +0300

    Add class UpdatePersonService
    Add class ru.dreremin.predefense.registration.sys.services.person.
    	.UpdatePersonService.
    Add fields this class to ru.dreremin.predefense.registration.sys.services
    	.student.UpdateStudentService and ru.dreremin.predefense.registration.sys
    	.services.teacher.UpdateTeacherService.

--- src/main/java/ru/dreremin/predefense/registration/sys/services/student/UpdateStudentService.java
+++ src/main/java/ru/dreremin/predefense/registration/sys/services/student/UpdateStudentService.java
@@ -3,7 +3,6 @@ package ru.dreremin.predefense.registration.sys.services.student;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,17 +13,13 @@ import ru.dreremin.predefense.registration.sys.exceptions
 		 .EntitiesMismatchException;
 import ru.dreremin.predefense.registration.sys.exceptions
 		 .NotReadableRequestParameterException;
-import ru.dreremin.predefense.registration.sys.models.Actor;
-import ru.dreremin.predefense.registration.sys.models.Email;
-import ru.dreremin.predefense.registration.sys.models.Person;
 import ru.dreremin.predefense.registration.sys.models.Student;
 import ru.dreremin.predefense.registration.sys.models.StudentCommission;
-import ru.dreremin.predefense.registration.sys.repositories.ActorRepository;
-import ru.dreremin.predefense.registration.sys.repositories.EmailRepository;
-import ru.dreremin.predefense.registration.sys.repositories.PersonRepository;
 import ru.dreremin.predefense.registration.sys.repositories
 		 .StudentCommissionRepository;
 import ru.dreremin.predefense.registration.sys.repositories.StudentRepository;
+import ru.dreremin.predefense.registration.sys.services.person
+		 .UpdatePersonService;
 
 @RequiredArgsConstructor
 @Service
@@ -32,58 +27,19 @@ public class UpdateStudentService {
 
 	private final StudentRepository studentRepository;
 	
-	private final ActorRepository actorRepository;
-	
-	private final PersonRepository personRepository;
-	
-	private final EmailRepository emailRepository;
-	
-	private final PasswordEncoder passwordEncoder;
-	
 	private final StudentCommissionRepository studentCommissionRepository;
+	
+	private final UpdatePersonService updatePersonService;
 	
 	@Transactional(isolation = Isolation.SERIALIZABLE, rollbackFor = {
 			EntityNotFoundException.class, 
 			NotReadableRequestParameterException.class,
 			EntitiesMismatchException.class})
 	public void updateStudent(long actorId, StudentRequestDto dto) {
+
+		long personId = updatePersonService.updatePerson(actorId, dto);
 		
-		Actor actor = actorRepository.findById(actorId).orElseThrow(
-				() -> new EntityNotFoundException(
-						"User with this ID does not exist"));
-		
-		if (dto.getLogin() != null) {
-			actor.setLogin(dto.getLogin());
-		}
-		if (dto.getPassword() != null) {
-			actor.setPassword(passwordEncoder.encode(dto.getPassword()));
-		}
-		
-		Person person = personRepository.findByActorId(actorId)
-				.orElseThrow(() -> new EntityNotFoundException(
-						"Person with this user ID does not exist"));
-		
-		if (dto.getLastName() != null) {
-			person.setLastName(dto.getLastName());
-		}
-		if (dto.getFirstName() != null) {
-			person.setFirstName(dto.getLastName());
-		}
-		if (dto.getPatronymic() != null) {
-			person.setPatronymic(dto.getPatronymic());
-		}
-		
-		Email email = emailRepository.findByPersonId(person.getId())
-				.orElseThrow(() -> new EntityNotFoundException(
-						"Email for this person does not exist"));
-		
-		if (dto.getEmail() != null 
-				&& !dto.getEmail().equals(email.getAddress())) {
-			emailValidation(dto.getEmail());
-			email.setAddress(dto.getEmail());
-		}
-		
-		Student student = studentRepository.findByPersonId(person.getId())
+		Student student = studentRepository.findByPersonId(personId)
 				.orElseThrow(() -> new EntityNotFoundException(
 						"Student with this user ID does not exist"));
 		
@@ -96,15 +52,6 @@ public class UpdateStudentService {
 		}
 		if (dto.getStudyType() != null) {
 			student.setStudyType(dto.getStudyType());
-		}
-	}
-	
-	private void emailValidation(String address) {
-		if (!address.matches(".+@.+\\..{1,3}") 
-				|| address.length() < 8 
-				|| address.length() > 40) {
-			throw new NotReadableRequestParameterException(
-					"Invalid format request body field \"email\"");
 		}
 	}
 	
--- src/main/java/ru/dreremin/predefense/registration/sys/services/teacher/UpdateTeacherService.java
+++ src/main/java/ru/dreremin/predefense/registration/sys/services/teacher/UpdateTeacherService.java
@@ -1,21 +1,16 @@
 package ru.dreremin.predefense.registration.sys.services.teacher;
 
 import javax.persistence.EntityNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import ru.dreremin.predefense.registration.sys.dto.request.TeacherRequestDto;
-import ru.dreremin.predefense.registration.sys.models.Actor;
-import ru.dreremin.predefense.registration.sys.models.Email;
-import ru.dreremin.predefense.registration.sys.models.Person;
 import ru.dreremin.predefense.registration.sys.models.Teacher;
-import ru.dreremin.predefense.registration.sys.repositories.ActorRepository;
-import ru.dreremin.predefense.registration.sys.repositories.EmailRepository;
-import ru.dreremin.predefense.registration.sys.repositories.PersonRepository;
 import ru.dreremin.predefense.registration.sys.repositories.TeacherRepository;
+import ru.dreremin.predefense.registration.sys.services.person
+		 .UpdatePersonService;
 
 @RequiredArgsConstructor
 @Service
@@ -23,53 +18,15 @@ public class UpdateTeacherService {
 
 	private final TeacherRepository teacherRepository;
 	
-	private final ActorRepository actorRepository;
-	
-	private final PersonRepository personRepository;
-	
-	private final EmailRepository emailRepository;
-	
-	private final PasswordEncoder passwordEncoder;
+	private final UpdatePersonService updatePersonService;
 	
 	@Transactional(isolation = Isolation.SERIALIZABLE, rollbackFor = {
 			EntityNotFoundException.class})
 	public void updateTeacher(long actorId, TeacherRequestDto dto) {
 		
-		Actor actor = actorRepository.findById(actorId).orElseThrow(
-				() -> new EntityNotFoundException(
-						"User with this ID does not exist"));
+		long personId = updatePersonService.updatePerson(actorId, dto);
 		
-		if (dto.getLogin() != null) {
-			actor.setLogin(dto.getLogin());
-		}
-		if (dto.getPassword() != null) {
-			actor.setPassword(passwordEncoder.encode(dto.getPassword()));
-		}
-		
-		Person person = personRepository.findByActorId(actorId)
-				.orElseThrow(() -> new EntityNotFoundException(
-						"Person with this user ID does not exist"));
-		
-		if (dto.getLastName() != null) {
-			person.setLastName(dto.getLastName());
-		}
-		if (dto.getFirstName() != null) {
-			person.setFirstName(dto.getLastName());
-		}
-		if (dto.getPatronymic() != null) {
-			person.setPatronymic(dto.getPatronymic());
-		}
-		
-		Email email = emailRepository.findByPersonId(person.getId())
-				.orElseThrow(() -> new EntityNotFoundException(
-						"Email for this person does not exist"));
-		
-		if (dto.getEmail() != null 
-				&& !dto.getEmail().equals(email.getAddress())) {
-			email.setAddress(dto.getEmail());
-		}
-		
-		Teacher teacher = teacherRepository.findByPersonId(person.getId())
+		Teacher teacher = teacherRepository.findByPersonId(personId)
 				.orElseThrow(() -> new EntityNotFoundException(
 						"Teacher with this user ID does not exist"));
 		
